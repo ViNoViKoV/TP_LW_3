629bd9d3b66793de0a6b0cf4fa5225f651aa2e18
MainVik <w.a.novikow@gmail.com>
2023-09-23 03:22:19 +0300

    Добавил функционал для графика температур. Работа над вариантом 3 окончена

---
+++ TemperatureForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace TP_LW_3_c1_n72
+{
+    partial class TemperatureForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series19 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series20 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series21 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OpenFileButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(207, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 37);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Температура за август";
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(385, 168);
+            this.chart1.Name = "chart1";
+            series19.ChartArea = "ChartArea1";
+            series19.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series19.LabelForeColor = System.Drawing.Color.Blue;
+            series19.Legend = "Legend1";
+            series19.Name = "Минимальная темп";
+            series20.ChartArea = "ChartArea1";
+            series20.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series20.LabelForeColor = System.Drawing.Color.Red;
+            series20.Legend = "Legend1";
+            series20.Name = "Максимальная темп";
+            series21.ChartArea = "ChartArea1";
+            series21.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series21.LabelForeColor = System.Drawing.Color.Gold;
+            series21.Legend = "Legend1";
+            series21.Name = "Среднаяя темп";
+            this.chart1.Series.Add(series19);
+            this.chart1.Series.Add(series20);
+            this.chart1.Series.Add(series21);
+            this.chart1.Size = new System.Drawing.Size(416, 287);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(594, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(194, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(594, 136);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(194, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(590, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(198, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Самый сильный перепад";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(426, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 43);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Температура (градусы Цельсия)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(480, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Дата";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 104);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(358, 334);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // OpenFileButton
+            // 
+            this.OpenFileButton.Location = new System.Drawing.Point(12, 59);
+            this.OpenFileButton.Name = "OpenFileButton";
+            this.OpenFileButton.Size = new System.Drawing.Size(358, 39);
+            this.OpenFileButton.TabIndex = 7;
+            this.OpenFileButton.Text = "Открыть файл";
+            this.OpenFileButton.UseVisualStyleBackColor = true;
+            this.OpenFileButton.Click += new System.EventHandler(this.OpenFileButton_Click);
+            // 
+            // TemperatureForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OpenFileButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Name = "TemperatureForm";
+            this.Text = "TemperatureForm";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button OpenFileButton;
+    }
+}
---
+++ TemperatureForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using ExcelDataReader;
+
+namespace TP_LW_3_c1_n72
+{
+    public partial class TemperatureForm : Form
+    {
+        //Объявление имени файла
+        private string filename = string.Empty;
+        //здесь объвление места под файл ексель
+        private DataTableCollection dataTable = null;
+        public TemperatureForm()
+        {
+            InitializeComponent();
+        }
+        //Метод проверки на выбор файла excel
+        private bool IsExcelFile(string filePath)
+        {
+            string extension = Path.GetExtension(filePath);
+            return extension.Equals(".xls") || extension.Equals(".xlsx") || extension.Equals(".xlsm");
+        }
+        // Метод для чтения данных из файла Excel
+        private DataTable OpenExcelFile(string path)
+        {
+            // Открытие потока для чтения выбранного файла Excel
+            FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            // Создание объекта для чтения данных из файла Excel
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+            // Чтение данных из файла Excel и преобразование их в объект DataSet
+            DataSet dataSet = reader.AsDataSet(new ExcelDataSetConfiguration
+            {
+                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = false
+                }
+            });
+            DataTable table = dataSet.Tables[0];
+            dataGridView1.DataSource = table;
+            return table;
+        }
+
+        private void DrawCharts(DataTable table)
+        {
+            // Добавление данных
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+                // Данные находятся во втором и третьем столбцах таблицы
+                double x = i; // Простое приращение для координаты X для равномерности графика
+                double y1 = Convert.ToDouble(table.Rows[i][1]);
+                double y2 = Convert.ToDouble(table.Rows[i][2]);
+                double y3 = Convert.ToDouble(table.Rows[i][3]);
+                //графики получают данные
+                chart1.Series[0].Points.AddXY(x, y1);
+                chart1.Series[1].Points.AddXY(x, y2);
+                chart1.Series[2].Points.AddXY(x, y3);
+                // Подпись дат из графика
+                chart1.Series[0].Points[i].AxisLabel = (table.Rows[i][0]).ToString().Substring(0, 10);
+            }
+        }
+
+        private void FindLiftDrop(DataTable table)
+        {
+            double maxDeviation = 0;
+            string Date = "";
+            //Максимальный перепад средней температуры
+            for (int i = 1; i < table.Rows.Count; i++)
+            {
+                double currDev = Convert.ToDouble(table.Rows[i][2]) - Convert.ToDouble(table.Rows[i - 1][2]);
+                if (currDev > Math.Abs(maxDeviation))
+                {
+                    maxDeviation = currDev;
+                    Date = Convert.ToString(table.Rows[i][0]);
+                }
+            }
+            //вывод в форму
+            textBox1.Text = Math.Round(maxDeviation, 0).ToString();
+            textBox2.Text = Date.Substring(0, 10); ;
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+            openFileDialog.Title = "Выберите файл excel";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                if (IsExcelFile(filePath))
+                {
+                    DataTable table = OpenExcelFile(filePath);
+                    //PlotGraph(table);
+                    DrawCharts(table);
+                    FindLiftDrop(table);
+                }
+                else
+                {
+                    MessageBox.Show("Выбран неверный файл.");
+                }
+            }
+        }
+
+    }
+}
